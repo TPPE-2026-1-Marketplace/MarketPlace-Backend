6f593ee6901d3dd7e1f70fe034cc04082496015a
Yzabella <yzabellamiranda4@gmail.com>
2026-04-21 15:06:17 -0300

    só um ajuste da camada + mvc

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './entities/user.entity';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService {
