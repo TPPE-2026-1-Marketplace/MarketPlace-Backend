3df1a8ddab35a57fccfd75a4fb60c7f6df4a6219
Bruno Braganca dos Reis <brunobragancadosreis@gmail.com>
2026-05-17 17:47:43 -0300

    feat: implementing person crud

--- src/people/people.module.ts
+++ src/people/people.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from './entities/person.entity';
 import { PeopleController } from './people.controller';
 import { PeopleService } from './people.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Person])],
   controllers: [PeopleController],
-  providers: [PeopleService]
+  providers: [PeopleService],
+  // Exporta o service para que `auth` (D0) e `employees` (D2) consigam usá-lo.
+  exports: [PeopleService],
 })
 export class PeopleModule {}
