c1f7370053280ed67a32c070623d397754e0f2f5
Bruno Braganca dos Reis <brunobragancadosreis@gmail.com>
2026-05-18 18:12:12 -0300

    fix: duplication in roles

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -27,4 +27,5 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from '../enums/role.enum';
 
-export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
