3ba79765ebeaa284cda6ca3e4e0071b987eff039
Bruno Braganca dos Reis <brunobragancadosreis@gmail.com>
2026-05-19 15:49:18 -0300

    fix: updating value for nome

--- src/categories/dtos/create-category.dto.ts
+++ src/categories/dtos/create-category.dto.ts
@@ -2,7 +2,7 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 export const CreateCategorySchema = z.object({
-  nome: z.string().min(1).max(120),
+  nome: z.string().min(1).max(80),
 });
 
 export class CreateCategoryDto extends createZodDto(CreateCategorySchema) {}
