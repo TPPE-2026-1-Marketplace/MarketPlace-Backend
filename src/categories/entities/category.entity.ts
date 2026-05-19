e239ffd58d023ea917ef0893a9d549730ba8cb75
Pabloserrapxx <pabloserrapx@gmail.com>
2026-05-19 00:08:10 -0300

    feat: implement categories module

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,10 @@
-export class CategoryEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  idCategoria: number;
+
+  @Column({ type: 'varchar', length: 120 })
+  nome: string;
+}
