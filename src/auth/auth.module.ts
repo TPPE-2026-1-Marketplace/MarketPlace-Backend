6cf53015c501b11ff59e6fb204f92804b1daae40
Bruno Braganca dos Reis <brunobragancadosreis@gmail.com>
2026-05-21 13:14:36 -0300

    refactor: implementing peoplemodule instead of forfeature person

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Person } from '../people/entities/person.entity';
+import { Employee } from '../employees/entities/employee.entity';
+import { PeopleModule } from '../people/people.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Person]),
+    PeopleModule,
+    TypeOrmModule.forFeature([Employee]),
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
