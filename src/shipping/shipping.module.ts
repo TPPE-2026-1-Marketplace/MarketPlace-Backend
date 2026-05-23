94516b33dfdb55464294d3e36c45266803f407da
Bruno Braganca dos Reis <brunobragancadosreis@gmail.com>
2026-05-23 20:30:12 -0300

    feat(shipping): integração via Melhor Envio com OAuth2 (closes #75)

--- src/shipping/shipping.module.ts
+++ src/shipping/shipping.module.ts
@@ -1,17 +1,19 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
+import { MELHOR_ENVIO_TIMEOUT_MS } from '../common/constants';
+import { MelhorEnvioTokenManager } from './auth/melhor-envio-token-manager';
 import { ShippingController } from './shipping.controller';
 import { ShippingService } from './shipping.service';
 
 @Module({
   imports: [
     HttpModule.register({
-      timeout: 5_000,
+      timeout: MELHOR_ENVIO_TIMEOUT_MS,
     }),
   ],
   controllers: [ShippingController],
-  providers: [ShippingService],
+  providers: [MelhorEnvioTokenManager, ShippingService],
   exports: [ShippingService],
 })
 export class ShippingModule {}
