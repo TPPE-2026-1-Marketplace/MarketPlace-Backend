0f519833d88ec96df52d585782c8318e2dde8a7a
Ana-Luiza-SC <ssoares.analuiza@gmail.com>
2026-06-22 10:53:30 -0300

    fix: corrige enable

--- src/main.ts
+++ src/main.ts
@@ -21,10 +21,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   // Libera o frontend (Vite) a consumir a API a partir de outra origem.
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://seu-frontend.onrender.com'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'http://localhost:5173', 'https://seu-frontend.onrender.com'],
+  //   credentials: true,
+  // });
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('API DK Fashion')
